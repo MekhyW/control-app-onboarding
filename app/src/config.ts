19256642ce97d02aad403e95af58c593f1aa7009
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-07-02 15:11:39 -0300

    locales

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from '@tok/generation';
 
 export default defineConfig({
-  // If you want to add language/currency localization – see ./examples/meditation as reference
-
+  theme: 'dark',
+  locale: {
+    fallback: 'en',
+    pt: import('./locales/pt.json'),
+    en: import('./locales/en.json'),
+  },
   pages: [
     {
       slides: [
-        // intro
+        // welcome
         {
           media: {
             type: 'sticker',
@@ -15,13 +19,12 @@ export default defineConfig({
           },
           shape: 'square',
           pagination: 'count',
-          title: 'Welcome to Telegram Onboarding Kit',
-          description:
-            "Create stunning onboarding and paywall for your Telegram Bot using the full power of Mini Apps<br><br>It's <b>simple</b>, <b>fast</b>, highly <b>customizable</b> and <a href='https://github.com/Easterok/telegram-onboarding-kit' target='_blank'>open-source</a>!",
-          button: 'Next',
+          title: '_s1.title',
+          description: "_s1.description",
+          button: '_s1.button',
         },
 
-        // image
+        // expressions
         {
           media: {
             type: 'image',
@@ -35,7 +38,7 @@ export default defineConfig({
           button: 'Next',
         },
 
-        // sticker
+        // playing music
         {
           media: {
             type: 'sticker',
@@ -50,7 +53,7 @@ export default defineConfig({
           button: 'Next',
         },
 
-        // form
+        // sound effects
         {
           extends: 'form', // note, it's important to extend from 'form' here
           media: {
@@ -82,7 +85,7 @@ export default defineConfig({
           button: 'Next',
         },
 
-        // video
+        // voice changer
         {
           media: {
             type: 'video',
@@ -98,7 +101,7 @@ export default defineConfig({
           button: 'Next',
         },
 
-        // list
+        // lights
         {
           media: {
             type: 'sticker',
@@ -139,7 +142,7 @@ export default defineConfig({
           button: 'Next',
         },
 
-        // "everything is customizable" slide
+        // assistant (cookiebot)
         {
           media: {
             type: 'sticker',
@@ -159,7 +162,7 @@ export default defineConfig({
           button: 'Super-Duper Next',
         },
 
-        // slide with other features
+        // app installation
         {
           media: {
             type: 'sticker',
@@ -182,79 +185,14 @@ export default defineConfig({
           button: 'Next',
         },
 
-        // go to paywall slide
+        // fake terms
         {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_knife.tgs'),
-            size: 250,
-          },
-          shape: 'square',
-          pagination: 'count',
-          textAlign: 'center',
-          title: 'But onboarding slides are not enough...',
-          description: "Let's go to Paywall",
-          button: {
-            content: 'Go to Paywall',
-            to: '/paywall',
-          },
-        },
-      ],
-    },
-
-    // paywall
-    {
-      extends: 'paywall',
-      path: '/paywall',
-      media: {
-        type: 'sticker',
-        src: import('./assets/stickers/duck_money.tgs'),
-        size: 150,
-      },
-      shape: 'square',
-      title: 'Your beautiful Paywall',
-      list: [
-        'Adjustable product cards',
-        '<b>👛 Wallet Pay</b> and <b>Telegram Payments</b> ready. Add custom methods easily',
-        'Subscriptions or One-time payments',
-      ],
-      products: [
-        {
-          id: '1_month_subscription',
-          title: '1 month subscription',
-          description: '2$/month',
-          discount: '',
-          price: 2,
-        },
-        {
-          id: '1_year_subscription',
-          title: '1 year subscription',
-          description: '1$/month',
-          discount: 'Discount 50%',
-          price: 12,
-        },
-        {
-          id: 'lifetime_access',
-          title: 'Lifetime access',
-          description: '20$ once',
-          discount: 'Best offer',
-          price: 20,
-        },
-      ],
-      mainButtonText: 'Buy for {price}',
-      popup: {
-        // popup for payment methods choice
-        type: 'web',
-      },
-      links: [
-        {
-          text: 'Privacy policy',
-          href: 'https://google.com',
-        },
-        {
-          text: 'Terms of use',
-          href: 'https://google.com',
-        },
+          extends: 'paywall',
+          title: 'Paywall',
+          description:
+            'Paywall is a page, where user can pay for access to your product',
+          button: 'Buy'
+        }
       ],
     },
   ],
