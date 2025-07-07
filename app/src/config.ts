0f40a518d01b58cb6809dc8909740ba80f4bd4e6
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-07-07 14:25:00 -0300

    Completed texts

--- app/src/config.ts
+++ app/src/config.ts
@@ -14,9 +14,8 @@ export default defineConfig({
         // welcome
         {
           media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_hello.tgs'),
-            size: 250,
+            type: 'image',
+            src: import('./assets/img/classified.webp'),
           },
           shape: 'square',
           pagination: 'count',
@@ -31,7 +30,7 @@ export default defineConfig({
             type: 'image',
             src: import('./assets/img/expressions.webp'),
           },
-          shape: 'square',
+          shape: 'stacked',
           pagination: 'count',
           title: '_s2.title',
           description: "_s2.description",
@@ -45,7 +44,7 @@ export default defineConfig({
             src: import('./assets/videos/musicplayer.mp4'),
             poster: import('./assets/img/musicplayer.webp'),
           },
-          shape: 'square',
+          shape: 'rounded',
           pagination: 'count',
           title: '_s3.title',
           description: "_s3.description",
@@ -54,31 +53,13 @@ export default defineConfig({
 
         // sound effects
         {
-          extends: 'form', // note, it's important to extend from 'form' here
           media: {
             type: 'video',
             src: import('./assets/videos/soundboard.mp4'),
             poster: import('./assets/img/soundboard.webp'),
           },
-          shape: 'square',
+          shape: 'stacked',
           pagination: 'count',
-          form: [
-            {
-              id: 'text_from_form',
-              placeholder: 'Text input',
-              type: 'text',
-            },
-            {
-              id: 'number_from_form',
-              placeholder: 'Number input',
-              type: 'number',
-            },
-            {
-              id: 'checkbox_from_form',
-              placeholder: 'Checkbox',
-              type: 'checkbox',
-            },
-          ],
           title: '_s4.title',
           description: "_s4.description",
           button: '_s4.button',
@@ -105,34 +86,8 @@ export default defineConfig({
             src: import('./assets/videos/lights.mp4'),
             poster: import('./assets/img/lights.webp'),
           },
-          shape: 'square',
+          shape: 'stacked',
           pagination: 'count',
-          list: [
-            {
-              media: {
-                type: 'icon',
-                src: import('./assets/icons/guide.svg'),
-                size: 30,
-              },
-              text: 'Some cool feature',
-            },
-            {
-              media: {
-                type: 'icon',
-                src: import('./assets/icons/track.svg'),
-                size: 30,
-              },
-              text: 'Some very cool feature',
-            },
-            {
-              media: {
-                type: 'icon',
-                src: import('./assets/icons/time.svg'),
-                size: 30,
-              },
-              text: 'Some extremely cool feature',
-            },
-          ],
           title: '_s6.title',
           description: "_s6.description",
           button: '_s6.button',
@@ -144,14 +99,9 @@ export default defineConfig({
             type: 'image',
             src: import('./assets/img/assistant.webp'),
           },
-          shape: 'square',
+          shape: 'stacked',
           pagination: 'count',
           textAlign: 'center',
-          list: [
-            '<b>CSS styles</b>: extend primary colors from Telegram or set yours',
-            'Button text and actions (look down)',
-            'Use our carefully crafted <b>presets</b> or easily create your own',
-          ],
           title: '_s7.title',
           description: "_s7.description",
           button: '_s7.button',
@@ -163,38 +113,16 @@ export default defineConfig({
             type: 'image',
             src: import('./assets/img/app.webp'),
           },
-          shape: 'square',
+          shape: 'rounded',
           pagination: 'count',
           list: [
-            'One-click 0$ <b>deploy</b> on GitHub Pages',
-            'Language and currency localization',
-            'Buttons with <b>haptic</b> feedback',
-            'Content pre-loading for high speed',
-            '<b>Low-code</b> approach to building onboardings',
-            'Many examples/presets',
-            "And many more... (see <a href='https://github.com/Easterok/telegram-onboarding-kit' target='_blank'>GitHub</a>)",
+            '_s8step1',
+            '_s8step2',
+            '_s8step3',
           ],
           title: '_s8.title',
           description: "_s8.description",
           button: '_s8.button',
-        },
-
-        // fake terms
-        {
-          extends: 'paywall',
-          title: '_s9.title',
-          description: '_s9.description',
-          links: [
-            {
-              text: 'Link 1',
-              href: 'https://google.com'
-            },
-            {
-              text: 'Link 2',
-              href: 'https://google.com'
-            }
-          ],
-          mainButtonText: '_s9.button',
         },
       ],
     },
